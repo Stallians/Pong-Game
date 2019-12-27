22b489d7b67d7491e333ce1b842976a500ac2ba5
Ashish Agrawal <ashish.agrawal1502@gmail.com>
2019-12-27 22:04:39 +0530

    Part 2: Add 2 paddles and the ball

--- Pong.py
+++ Pong.py
@@ -1,5 +1,5 @@
 # Simple Pong Game in Python3 for Beginners
-# Part 1: Game Window
+# Part 2: Game Objects
 
 import turtle
 
@@ -8,6 +8,33 @@ window.title('Pong by @stallians')
 window.bgcolor("black")
 window.setup(width=800, height=600)
 window.tracer(0)
+
+# paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape('square')
+paddle_a.shapesize(stretch_len=1, stretch_wid=5)
+paddle_a.color("white")
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape('square')
+paddle_b.shapesize(stretch_len=1, stretch_wid=5)
+paddle_b.color("white")
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape('square')
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+
 
 while True:
     window.update()
